4cfd9b967ef03b6ca328727883ed2beba48d2a87
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2017-05-08 16:03:43 -0400

    Init

---
+++ Roll Bol/Assets/Scripts/Rotateur.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotateur : MonoBehaviour {
+	void Update () {
+		
+		transform.Rotate (new Vector3(30,60,90) * Time.deltaTime);
+	}
+}
